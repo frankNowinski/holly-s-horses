d258621d7d925a1c9b83910851019c53358c1f8d
frankNowinski <nowinski.frank@gmail.com>
2016-07-15 18:45:45 -0400

    added picture

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,13 @@
 //= require angular-ui-router
 //= require angular-resource
 //= require angular-rails-templates
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui
 //= require_tree .
+
+$(function(){
+  $('#home-page-picture').mouseover(function(){
+    $('.image-caption').removeClass('hide');
+  })
+});
